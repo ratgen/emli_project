3fe8b07fc62232991a1350dec38249bb326a9ae1
Peter Heilbo Ratgen <peter@pratgen.dk>
2023-04-27 15:31:12 +0200

    Complete publish script

--- raspberry_pi/pico_serial_input.sh
+++ raspberry_pi/pico_serial_input.sh
@@ -3,13 +3,18 @@
 stty -F /dev/ttyACM0 115200
 exec < /dev/ttyACM0
 
+host=localhost
+port=1883
+username=team13
+password=password
+
 # Wait for temperature string
 while true; do
   IFS=',' read -ra line 
-
-  mosquitto_pub -h localhost -p 1883 -t team13/sensors/plant_alarm -m ${line[0]}
-  mosquitto_pub -h localhost -p 1883 -t team13/sensors/pump_alarm -m ${line[1]}
-  mosquitto_pub -h localhost -p 1883 -t team13/sensors/moisture -m ${line[2]}
-  mosquitto_pub -h localhost -p 1883 -t team13/sensors/light -m  ${line[3]}
-
+  if [ ${#line[@]} != 0 ]; then
+    mosquitto_pub -h $host -p $port -u $username -P $password -t team13/sensors/plant_alarm -m ""${line[0]}
+    mosquitto_pub -h $host -p $port -u $username -P $password -t team13/sensors/pump_alarm -m ""${line[1]}
+    mosquitto_pub -h $host -p $port -u $username -P $password -t team13/sensors/moisture -m ""${line[2]}
+    mosquitto_pub -h $host -p $port -u $username -P $password -t team13/sensors/light -m ""${line[3]}
+  fi
 done
