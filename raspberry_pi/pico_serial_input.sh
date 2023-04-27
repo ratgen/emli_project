449883a442ddb356d09fa6bc699844d8bf993f65
Peter Heilbo Ratgen <peter@pratgen.dk>
2023-04-27 14:48:46 +0200

    serial stuff

--- raspberry_pi/pico_serial_input.sh
+++ raspberry_pi/pico_serial_input.sh
@@ -1,1 +1,15 @@
-#! /bin/bash
+#!/bin/bash
+
+stty -F /dev/ttyACM0 115200
+exec < /dev/ttyACM0
+
+# Wait for temperature string
+while true; do
+  IFS=',' read -ra line 
+
+  mosquitto_pub -h localhost -p 1883 -t team13/sensors/plant_alarm -m ${line[0]}
+  mosquitto_pub -h localhost -p 1883 -t team13/sensors/pump_alarm -m ${line[1]}
+  mosquitto_pub -h localhost -p 1883 -t team13/sensors/moisture -m ${line[2]}
+  mosquitto_pub -h localhost -p 1883 -t team13/sensors/light -m  ${line[3]}
+
+done
