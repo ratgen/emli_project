55b2b7a255f667922ebd7a60ca7e76dc25f7565f
Peter Heilbo Ratgen <peter@pratgen.dk>
2023-05-15 11:32:05 +0200

    add pump_write 	- add systemd service

--- raspberry_pi/pico_serial_input.sh
+++ raspberry_pi/pico_serial_input.sh
@@ -11,9 +11,17 @@ password=password
 # Wait for temperature string
 while true; do
   IFS=',' read -ra line 
+  
+  pump_alarm=${line[1]}
+  if [ "$pump_alarm" == "0" ]; then
+    pump_alarm=1
+  else
+    pump_alarm=0
+  fi
+
   if [ ${#line[@]} != 0 ]; then
     mosquitto_pub -h $host -p $port -u $username -P $password -t team13/sensors/plant_alarm -m ""${line[0]}
-    mosquitto_pub -h $host -p $port -u $username -P $password -t team13/sensors/pump_alarm -m ""${line[1]}
+    mosquitto_pub -h $host -p $port -u $username -P $password -t team13/sensors/pump_alarm -m ""$pump_alarm
     mosquitto_pub -h $host -p $port -u $username -P $password -t team13/sensors/moisture -m ""${line[2]}
     mosquitto_pub -h $host -p $port -u $username -P $password -t team13/sensors/light -m ""${line[3]}
   fi
