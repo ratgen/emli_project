f433e185c13202c2aebf0e9ca98cd1017081c37d
Peter Heilbo Ratgen <peter@pratgen.dk>
2023-06-04 14:37:13 +0200

    use pipes

--- raspberry_pi/healh_publish.sh
+++ raspberry_pi/healh_publish.sh
@@ -6,9 +6,9 @@ username=team13
 password=password
 
 while true; do
-  mosquitto_pub -h $host -p $port -u $username -P $password -t team13/raspberry/health/available_ram_percentage -m "$(/bin/bash /home/pi/documents/emli_project/raspberry_pi/health/available_ram.sh)"
-  mosquitto_pub -h $host -p $port -u $username -P $password -t team13/raspberry/health/cpu_temperature -m "$(/bin/bash /home/pi/documents/emli_project/raspberry_pi/health/cpu_temperature.sh)"
-  mosquitto_pub -h $host -p $port -u $username -P $password -t team13/raspberry/health/current_cpu_load_percentage -m "$(/bin/bash /home/pi/documents/emli_project/raspberry_pi/health/current_cpu_load.sh)"
-  mosquitto_pub -h $host -p $port -u $username -P $password -t team13/raspberry/health/disk_usage_percent -m "$(/bin/bash /home/pi/documents/emli_project/raspberry_pi/health/disk_percent.sh)"
+  /bin/bash /home/pi/documents/emli_project/raspberry_pi/health/available_ram.sh | mosquitto_pub -h $host -p $port -u $username -P $password -t team13/raspberry/health/available_ram_percentage -s
+  /bin/bash /home/pi/documents/emli_project/raspberry_pi/health/cpu_temperature.sh | mosquitto_pub -h $host -p $port -u $username -P $password -t team13/raspberry/health/cpu_temperature -s
+  /bin/bash /home/pi/documents/emli_project/raspberry_pi/health/current_cpu_load.sh | mosquitto_pub -h $host -p $port -u $username -P $password -t team13/raspberry/health/current_cpu_load_percentage -s
+  /bin/bash /home/pi/documents/emli_project/raspberry_pi/health/disk_percent.sh | mosquitto_pub -h $host -p $port -u $username -P $password -t team13/raspberry/health/disk_usage_percent -s
   sleep 2
 done
