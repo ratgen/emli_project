4d2ad6d95cc8add00b594689ec222321a222ed22
Peter Heilbo Ratgen <peter@pratgen.dk>
2023-06-05 15:22:13 +0200

    remove redundent echo

--- raspberry_pi/health/current_cpu_load.sh
+++ raspberry_pi/health/current_cpu_load.sh
@@ -1,3 +1,3 @@
 #/bin/bash
 
-echo $(ps aux | LC_NUMERIC="C" awk {'sum+=$3; printf("%f\n",sum) '} | tail -n 1 | cut -c -4)
+ps aux | LC_NUMERIC="C" awk {'sum+=$3; printf("%f\n",sum) '} | tail -n 1 | cut -c -4
